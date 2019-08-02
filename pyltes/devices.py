5fcfd5faf6608f6a470db2e182c9c7fda446fcb9
criddle858 <52388159+criddle858@users.noreply.github.com>
2019-08-02 17:48:10 -0600

    Update devices.py

--- pyltes/devices.py
+++ pyltes/devices.py
@@ -127,7 +127,7 @@ class UE(NetworkDevice):
             PL = 20 * math.log10( (4*math.pi*R) / lambda_val ) + s
 
             
-        print("R = ", R, ", PL = ", PL, "Psend = ", Psend)
+        print("R = ", R, ", PL = ", PL, "Psend = ", PSend)
         pRec = pSend - PL
         if(pRec > pSend):
             pRec = pSend
