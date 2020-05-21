e78fcfd07c8ababc68cba800b342520aadedf452
criddle858 <52388159+criddle858@users.noreply.github.com>
2020-05-21 10:34:19 -0700

    Update printer.py

--- pyltes/printer.py
+++ pyltes/printer.py
@@ -32,24 +32,23 @@ class Printer:
     def drawNetwork(self, filename, BS=True, UE=True, links=True, obstacles=True, fillMethod="SINR", colorMap = 'viridis', drawLegend=True, tilesInLine = 100, figSize = (8, 8), colorMinValue = None, colorMaxValue = None, outputFileFormat = ["png"]):
         main_draw = plt.figure(1, figsize=figSize)
         ax = main_draw.add_subplot(111)
-  
-        cm = plt.cm.get_cmap(colorMap)
-        ue = devices.UE()
-        imageMatrix = np.zeros((tilesInLine, tilesInLine))
-        d_x = round(self.parent.constraintAreaMaxX/tilesInLine)
-        d_y = round(self.parent.constraintAreaMaxY/tilesInLine)
-        for x in range(0, tilesInLine):
-            for y in range(0, tilesInLine):
-                ue.x = x * d_x
-                ue.y = y * d_y
-                if fillMethod == "SINR":
+        if fillMethod == "SINR":
+            cm = plt.cm.get_cmap(colorMap)
+            ue = devices.UE()
+            imageMatrix = np.zeros((tilesInLine, tilesInLine))
+            d_x = round(self.parent.constraintAreaMaxX/tilesInLine)
+            d_y = round(self.parent.constraintAreaMaxY/tilesInLine)
+            for x in range(0, tilesInLine):
+                for y in range(0, tilesInLine):
+                    ue.x = x * d_x
+                    ue.y = y * d_y
                     ue.connectToTheBestBS(self.parent.bs, self.parent.obstacles)
                     debug = 0
                     #if( (x % 10) == 0 and (y % 10) == 0 ):
                     #    debug = 1
                     SINR, _ = ue.calculateSINR(self.parent.bs, self.parent.obstacles, debug)
                     imageMatrix[y][x] = SINR
-
+                    
             if colorMinValue != None:
                 colorMin = colorMinValue
             else:
@@ -68,28 +67,31 @@ class Printer:
                 #cbar.ax.set_yticklabels(['0','1','2','>3'])
                 #cbar.set_label('# of contacts', rotation=270)
 
-#         elif fillMethod == "Sectors":
-#             cm = plt.cm.get_cmap(colorMap)
-#             ue = devices.UE()
-#             imageMatrix = np.zeros((tilesInLine, tilesInLine))
-#             d_x = round(self.parent.constraintAreaMaxX/tilesInLine)
-#             d_y = round(self.parent.constraintAreaMaxY/tilesInLine)
-#             for x in range(0, tilesInLine):
-#                 for y in range(0, tilesInLine):
-#                     RSSI_best = -1000
-#                     BS_best = -1
-#                     for bs in self.parent.bs:
-#                         ue.x = x * d_x
-#                         ue.y = y * d_y
+        elif fillMethod == "Sectors":
+            if colorMap == None:
+                cm = plt.cm.get_cmap("Paired")
+            else:
+                cm = plt.cm.get_cmap(colorMap)
+            ue = devices.UE()
+            imageMatrix = np.zeros((tilesInLine, tilesInLine))
+            d_x = round(self.parent.constraintAreaMaxX/tilesInLine)
+            d_y = round(self.parent.constraintAreaMaxY/tilesInLine)
+            for x in range(0, tilesInLine):
+                for y in range(0, tilesInLine):
+                    RSSI_best = -1000
+                    BS_best = -1
+                    for bs in self.parent.bs:
+                        ue.x = x * d_x
+                        ue.y = y * d_y
 
-#                         ue.connectedToBS = bs.ID
-#                         temp_RSSI, _ = ue.calculateSINR(self.parent.bs)
-#                         if temp_RSSI > RSSI_best:
-#                             RSSI_best = temp_RSSI
-#                             BS_best = bs.ID
+                        ue.connectedToBS = bs.ID
+                        temp_RSSI, _ = ue.calculateSINR(self.parent.bs)
+                        if temp_RSSI > RSSI_best:
+                            RSSI_best = temp_RSSI
+                            BS_best = bs.ID
 
-#                     imageMatrix[y][x] = BS_best
-#             plt.imshow(imageMatrix, origin='lower', extent=[0, self.parent.constraintAreaMaxX, 0, self.parent.constraintAreaMaxY], interpolation='nearest', cmap=cm)
+                    imageMatrix[y][x] = BS_best
+            plt.imshow(imageMatrix, origin='lower', extent=[0, self.parent.constraintAreaMaxX, 0, self.parent.constraintAreaMaxY], interpolation='nearest', cmap=cm)
 
         if BS == True:
             bs_x_locations = []
